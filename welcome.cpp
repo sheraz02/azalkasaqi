51b9ae88560da996f3d94a803ab75f90778a8721
azalkasaqi <azalkasaqi@azalkasaqi.Home>
2024-03-16 11:04:05 +0530

    fgbg

--- welcome.cpp
+++ welcome.cpp
@@ -1,36 +1,100 @@
 # include <iostream>
 using namespace std;
-int main(){
-    string name;
-    int age;
-    cout << "Your Name? ";
-    cin >> name;
-    cout << "Age? ";
-    cin >> age;
-    cout << "Hello, " << name << " You are " << age << " old.";\
+// int main(){
+//     string name;
+//     int age;
+//     cout << "Your Name? ";
+//     cin >> name;
+//     cout << "Age? ";
+//     cin >> age;
+//     cout << "Hello, " << name << " You are " << age << " old.";\
 
-    return 0;
-}
+// return 0;
+// }
 
 
-int main(){
-    double  RegularWages,
-            BasePayRate = 10,
-            RegularHours = 5,
-            OverTimeWages,
-            OverTimePayRate = 12,
-            OverTimeHours = 10,
-            TotalWages;
+// int main(){
+//     double  RegularWages,
+//             BasePayRate = 10,
+//             RegularHours = 5,
+//             OverTimeWages,
+//             OverTimePayRate = 12,
+//             OverTimeHours = 10,
+//             TotalWages;
         
     
-    RegularWages = BasePayRate * RegularHours;
+//     RegularWages = BasePayRate * RegularHours;
 
-    OverTimeWages = OverTimePayRate * OverTimeHours;
+//     OverTimeWages = OverTimePayRate * OverTimeHours;
 
-    TotalWages = RegularWages + OverTimeWages;
+//     TotalWages = RegularWages + OverTimeWages;
 
-    cout << "Total Earning this week..! " << TotalWages << endl;
+//     cout << "Total Earning this week..! " << TotalWages << endl;
 
-    return 0;
+//     return 0;
 
-}
+// }
+
+
+// int main(){
+//     double Monthlypay, PercentageContribution;
+
+//     Monthlypay = 6000;
+
+//     PercentageContribution = Monthlypay * 0.05;
+
+//     cout << "Your Contribution: " << PercentageContribution << endl;
+
+//     PercentageContribution = Monthlypay * 0.07;
+
+//     cout << "Contribution on 7%: " << PercentageContribution << endl;
+
+//     PercentageContribution = Monthlypay * 0.1;
+
+//     cout << "Contribution on 10%: " << PercentageContribution << endl;
+
+//     return 0;
+// }
+
+// int main(){
+//     double RegularPrice = 59.95, discount, SalePrice;
+        
+//         cout << "Regular price: $" << RegularPrice << endl;
+//         discount = RegularPrice * 0.20;
+
+//         cout << "Discount: $" << discount << endl;
+
+//         SalePrice = RegularPrice - discount;
+
+//         cout << "Sale price: $" << SalePrice << endl;
+
+//         return 0;
+// }
+
+
+
+//  int main()
+//  {
+//     int freeze = 32, boil = 212;
+//     freeze = 0;
+//     boil = 100;
+//     cout << freeze << endl << boil << endl;
+//     return 0;
+//  } 
+
+
+// int main()
+// {
+//     int x = 0, y = 2;
+//     x = y * 4;
+//     cout << x << endl << y << endl;
+//     return 0;
+//  } 
+
+int main()
+ {
+ int a, x = 23;
+ a = x % 2;
+ cout << x << endl << a << endl;
+ return 0;
+ }
